7f039c4ff7cda8590cc6928d5a4f8033fe9d7702
adithyan-sreeni <adithyansreeni8@gmail.com>
2025-02-24 01:58:11 +0530

    added lennnis scroll,modified the scroll bar , added Text animation to the title of FAQ

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,41 @@ import Loader from "./components/Loader";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import IndiviEvent from "./pages/IndiviEvent";
+//animations
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "@studio-freight/lenis";
+
+// Register ScrollTrigger plugin
+gsap.registerPlugin(TextPlugin, ScrollTrigger);
 
 function App() {
   const [loading, setLoading] = useState(true);
+
+  // Initialize Lenis for smooth scrolling
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2, // Duration of the scroll animation
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing function
+      smooth: true, // Enable smooth scrolling
+    });
+
+    // Update Lenis on each frame
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    // Cleanup Lenis on unmount
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
 
   useEffect(() => {
     //scroling to top
